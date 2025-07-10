3d4b638883130ae1d61854298653ffbd326bad36
Adam <nilings2018@outlook.com>
2025-07-11 03:00:14 +0800

    添加：加入按钮选择

---
+++ pages/WakeToSleep/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <view class="container">
+    <view class="title">入睡时间计算睡醒时间</view>
+    <view class="desc">请选择您准备入睡的时间：</view>
+    <picker mode="time" :value="sleepTime" @change="onTimeChange">
+      <view class="picker">
+        {{ sleepTime }}
+      </view>
+    </picker>
+    <view class="result" v-if="wakeTimes.length">
+      <view class="result-title">推荐醒来时间：</view>
+      <view class="sleep-time-list">
+        <view class="sleep-time" v-for="(time, idx) in wakeTimes" :key="idx">
+          {{ time }}
+        </view>
+      </view>
+    </view>
+    <view class="tips">
+      <text>每个睡眠周期为90分钟，建议成年人保证5-6个周期（7.5-9小时）。</text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sleepTime: '23:00',
+      sleepCycles: [6, 5, 4, 3],
+      wakeTimes: []
+    }
+  },
+  onLoad() {
+    this.calcWakeTimes();
+  },
+  methods: {
+    onTimeChange(e) {
+      this.sleepTime = e.detail.value;
+      this.calcWakeTimes();
+    },
+    calcWakeTimes() {
+      const [sleepHour, sleepMinute] = this.sleepTime.split(":").map(Number);
+      const sleepTotalMinutes = sleepHour * 60 + sleepMinute;
+      this.wakeTimes = this.sleepCycles.map(cycle => {
+        const wakeMinutes = sleepTotalMinutes + cycle * 90;
+        let hour = Math.floor((wakeMinutes) % 1440 / 60);
+        let minute = (wakeMinutes) % 60;
+        hour = hour < 10 ? '0' + hour : '' + hour;
+        minute = minute < 10 ? '0' + minute : '' + minute;
+        return `${hour}:${minute}`;
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 40rpx 20rpx;
+}
+.title {
+  font-size: 40rpx;
+  font-weight: bold;
+  margin-bottom: 30rpx;
+  text-align: center;
+}
+.desc {
+  font-size: 28rpx;
+  margin-bottom: 20rpx;
+}
+.picker {
+  background: #f5f5f5;
+  padding: 20rpx;
+  border-radius: 10rpx;
+  text-align: center;
+  font-size: 32rpx;
+  margin-bottom: 30rpx;
+}
+.result-title {
+  font-size: 30rpx;
+  font-weight: bold;
+  margin-bottom: 10rpx;
+}
+.sleep-time-list {
+  display: flex;
+  flex-direction: column;
+  gap: 10rpx;
+  margin-bottom: 20rpx;
+}
+.sleep-time {
+  background: #e0f7fa;
+  padding: 16rpx;
+  border-radius: 8rpx;
+  text-align: center;
+  font-size: 28rpx;
+}
+.tips {
+  color: #888;
+  font-size: 24rpx;
+  margin-top: 30rpx;
+  text-align: center;
+}
+</style> 
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,106 +1,51 @@
 <template>
-	<view class="container">
-	  <view class="title">理想入睡时间计算器</view>
-	  <view class="desc">请选择您希望醒来的时间：</view>
-	  <picker mode="time" :value="wakeTime" @change="onTimeChange">
-		<view class="picker">
-		  {{ wakeTime }}
-		</view>
-	  </picker>
-	  <view class="result" v-if="sleepTimes.length">
-		<view class="result-title">推荐入睡时间：</view>
-		<view class="sleep-time-list">
-		  <view class="sleep-time" v-for="(time, idx) in sleepTimes" :key="idx">
-			{{ time }}
-		  </view>
-		</view>
-	  </view>
-	  <view class="tips">
-		<text>每个睡眠周期为90分钟，建议成年人保证5-6个周期（7.5-9小时）。</text>
-	  </view>
-	</view>
-  </template>
-  
-  <script>
-  export default {
-	data() {
-	  return {
-		wakeTime: '07:00', // 默认醒来时间
-		sleepCycles: [6, 5, 4, 3], // 推荐周期数
-		sleepTimes: []
-	  }
-	},
-	onLoad() {
-	  this.calcSleepTimes();
-	},
-	methods: {
-	  onTimeChange(e) {
-		this.wakeTime = e.detail.value;
-		this.calcSleepTimes();
-	  },
-	  calcSleepTimes() {
-		// 解析醒来时间
-		const [wakeHour, wakeMinute] = this.wakeTime.split(":").map(Number);
-		const wakeTotalMinutes = wakeHour * 60 + wakeMinute;
-		this.sleepTimes = this.sleepCycles.map(cycle => {
-		  const sleepMinutes = wakeTotalMinutes - cycle * 90;
-		  let hour = Math.floor((sleepMinutes + 1440) % 1440 / 60);
-		  let minute = (sleepMinutes + 1440) % 60;
-		  // 补零
-		  hour = hour < 10 ? '0' + hour : '' + hour;
-		  minute = minute < 10 ? '0' + minute : '' + minute;
-		  return `${hour}:${minute}`;
-		});
-	  }
-	}
+  <view class="container">
+    <view class="title">睡眠时间计算器</view>
+    <button class="main-btn" @click="goToSleepToWake">睡醒时间计算入睡时间</button>
+    <button class="main-btn" @click="goToWakeToSleep">入睡时间计算睡醒时间</button>
+  </view>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToSleepToWake() {
+      uni.navigateTo({ url: '/pages/SleepToWake/index' });
+    },
+    goToWakeToSleep() {
+      uni.navigateTo({ url: '/pages/WakeToSleep/index' });
+    },
+    goToOldCalculator() {
+      uni.navigateTo({ url: '/pages/OldSleepCalculator/index' });
+    }
   }
-  </script>
-  
-  <style scoped>
-  .container {
-	padding: 40rpx 20rpx;
-  }
-  .title {
-	font-size: 40rpx;
-	font-weight: bold;
-	margin-bottom: 30rpx;
-	text-align: center;
-  }
-  .desc {
-	font-size: 28rpx;
-	margin-bottom: 20rpx;
-  }
-  .picker {
-	background: #f5f5f5;
-	padding: 20rpx;
-	border-radius: 10rpx;
-	text-align: center;
-	font-size: 32rpx;
-	margin-bottom: 30rpx;
-  }
-  .result-title {
-	font-size: 30rpx;
-	font-weight: bold;
-	margin-bottom: 10rpx;
-  }
-  .sleep-time-list {
-	display: flex;
-	flex-direction: column;
-	gap: 10rpx;
-	margin-bottom: 20rpx;
-  }
-  .sleep-time {
-	background: #e0f7fa;
-	padding: 16rpx;
-	border-radius: 8rpx;
-	text-align: center;
-	font-size: 28rpx;
-  }
-  .tips {
-	color: #888;
-	font-size: 24rpx;
-	margin-top: 30rpx;
-	text-align: center;
-  }
-  </style>
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 40rpx 20rpx;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.title {
+  font-size: 40rpx;
+  font-weight: bold;
+  margin-bottom: 60rpx;
+  text-align: center;
+}
+.main-btn {
+  width: 80%;
+  margin: 20rpx 0;
+  font-size: 32rpx;
+  padding: 30rpx 0;
+  border-radius: 12rpx;
+  background: #4fc08d;
+  color: #fff;
+  border: none;
+}
+</style>
   
